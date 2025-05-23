7bf66cfa8014a6a382d644bb55bc7744a8bd50cd
hamza <hamzaelmaaloumi2005@gmail.com>
2025-05-23 17:02:40 +0100

    setting backend apis and database

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,18 +1,25 @@
 import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "@/prisma/client";
 
 
-export function GET(request: NextRequest){
-    return NextResponse.json([
-        {id: 1, name: "hamza"},
-        {id: 2, name: "achraf"}
-    ])
+export async function GET(request: NextRequest){
+    const users = await prisma.$queryRaw`select * from user`;
+    return NextResponse.json(users, {status: 202});
 }
 
 export async function POST(request: NextRequest){
-    const body = await request.json();
-    if(!body.name){
-        return NextResponse.json({error: 'name is required'}, {status: 400})
-    }else{
-        return NextResponse.json({id: 1, name: "hamza"}, {status: 201})
+    try{
+        const body = await request.json();
+        if(!body.name || !body.email) return NextResponse.json({error: 'Both name and email are required'}, {status: 400})
+        const res: any = await prisma.$queryRaw`select * from user where email = ${body.email}`;
+        console.log(res);
+        if(res.length !== 0){
+            return NextResponse.json({error: "user already exists"}, {status: 400});
+        }
+
+        await prisma.$queryRaw`insert into user (email, name) values (${body.email}, ${body.name})`;
+        return NextResponse.json({success: "user inserted successfully"}, {status: 201});
+    }catch{
+        return NextResponse.json({error: 'an error happened'}, {status: 400});
     }
 }
